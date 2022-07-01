30c49041effd94fbed9441c30fd84bceedb78292
songulates <songl.ates.02@gmail.com>
2022-07-01 14:21:14 +0300

    degisti

--- Program.cs
+++ Program.cs
@@ -5,32 +5,23 @@ namespace console_programlama
     {
         static void Main(string[] args)//
         {
+            try
+            {
+                Console.WriteLine("sayi gri");
+                int sayi=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("girilen sayi "+sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: "+ ex.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+
+            }
            
-             try
-            {
-                
-                //int a=int.Parse(null);
-                //int a =int.Parse("test");
-                int b =int.Parse("11111111111111");
-               
-            }
-            catch(ArgumentNullException ex)
-            {
-                Console.WriteLine("boş değer girdiniz");
-                Console.WriteLine(ex);
-            }
-            catch(FormatException ex)
-            {
-                Console.WriteLine("veri tipi uygun değil");
-                Console.WriteLine(ex);
-
-            }
-            catch(OverflowException ex)
-            {
-                Console.WriteLine("çok büyük veya çok küçük değer girdiniz");
-                Console.WriteLine(ex);
-
-            }
 
 
         }
