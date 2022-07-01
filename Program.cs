37829f161d9463f8fc7961703e9e6823a872822d
songulates <songl.ates.02@gmail.com>
2022-07-01 14:11:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace console_programlama
+{ 
+    class Program
+    {
+        static void Main(string[] args)//
+        {
+           
+             try
+            {
+                
+                //int a=int.Parse(null);
+                //int a =int.Parse("test");
+                int b =int.Parse("11111111111111");
+               
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("boş değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("veri tipi uygun değil");
+                Console.WriteLine(ex);
+
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("çok büyük veya çok küçük değer girdiniz");
+                Console.WriteLine(ex);
+
+            }
+
+
+        }
+    }
+}
+
